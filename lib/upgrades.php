4a27d9def67d7a254127f7da77b88363692c38d2
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2017-06-07 18:24:03 +0200

    fix(upgrade): always check if upgrades are necessary
    Ensure that upgrade scripts are created whenever there are missing
    river entries in the feed table. This is necessary to ensure that
    the feed can be synced after the plugin is disabled for a certain
    period of time.

--- lib/upgrades.php
+++ lib/upgrades.php
@@ -13,35 +13,33 @@ if (!elgg_get_plugin_setting('upgrade_schema', 'hypeFeed')) {
 	if ($upgrade instanceof ElggUpgrade) {
 		$upgrade->delete();
 	}
-	
+
 	elgg_set_plugin_setting('upgrade_schema', time(), 'hypeFeed');
 }
 
 // Setup MySQL databases
 run_sql_script(dirname(dirname(__FILE__)) . '/install/mysql.sql');
 
-// Register upgrade scripts
-$path = 'admin/upgrades/feed/populate';
-$upgrade = new ElggUpgrade();
-$upgrade = $upgrade->getUpgradeFromPath($path);
-
-if (!$upgrade instanceof ElggUpgrade) {
-
-	$dbprefix = elgg_get_config('dbprefix');
-	$count = elgg_get_river([
-		'count' => true,
-		'wheres' => [
-			"NOT EXISTS (SELECT 1 FROM {$dbprefix}feeds
+$dbprefix = elgg_get_config('dbprefix');
+$count = elgg_get_river([
+	'count' => true,
+	'wheres' => [
+		"NOT EXISTS (SELECT 1 FROM {$dbprefix}feeds
 				WHERE id = rv.id)",
-		],
-	]);
+	],
+]);
 
-	if ($count) {
+if ($count) {
+	// Register upgrade scripts
+	$path = 'admin/upgrades/feed/populate';
+	$upgrade = new ElggUpgrade();
+	$upgrade = $upgrade->getUpgradeFromPath($path);
+	if (!$upgrade instanceof ElggUpgrade) {
 		$upgrade = new ElggUpgrade();
 		$upgrade->setPath($path);
 		$upgrade->title = elgg_echo('admin:upgrades:feed:populate');
 		$upgrade->description = elgg_echo('admin:upgrades:feed:populate:description');
-		$upgrade->is_completed = 0;
 		$upgrade->save();
 	}
+	$upgrade->is_completed = 0;
 }
